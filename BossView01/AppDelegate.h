56e15b299ce17bcb38f1f0eebc70065f4f891f80
1914833933 <student@MC.local>
2016-09-02 21:13:12 -0700

    于海龙
    创建工程

---
+++ BossView01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BossView01
+//
+//  Created by student on 16/9/2.
+//  Copyright © 2016年 s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BossView01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BossView01
+//
+//  Created by student on 16/9/2.
+//  Copyright © 2016年 s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
